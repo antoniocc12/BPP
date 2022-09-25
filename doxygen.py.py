c00066f0915aca7a86921cdd09f3f9ed1f0e6112
antonioccpro <toniprueba936@gmail.com>
2022-09-25 20:38:05 +0200

    Lección 3

---
+++ doxygen.py.py
@@ -0,0 +1,31 @@
+# from funciones_pytest import *
+
+# def test_1():
+#     assert exp_cuadrado(2) == 4
+
+# def test_2():
+#     assert resto(10, 3) == 1
+
+# def test_3():
+#     assert list_max([12, 34, 78, 23]) == 78
+
+# def test_4():
+#     assert list_max([12, 34, 78, 23]) == 23
+
+# def test_5():
+#     with pytest.raises(ZeroDivisionError):
+#         resultado = 10/0
+#         return resultado
+
+"""@package docstring
+Documentation for this module.
+
+More details.
+"""
+
+def func():
+    """Documentation for a function.
+
+    More details.
+    """
+    pass
